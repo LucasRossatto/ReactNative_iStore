af0eeef45b56ae0b33d23c2c6c70a26daf4a6439
LucasRossatto <154370022+LucasRossatto@users.noreply.github.com>
2024-11-04 16:34:11 -0300

    add Categorie template

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,52 +4,62 @@ import { StyleSheet, Text, View, Image } from "react-native";
 export default function Header() {
   return (
     <View style={style.container}>
-
-
-        <Image source={require("../assets/Icons/Vector.png")} style={style.apple} />
-        <Text style={style.tituloHeader}>iStore</Text>
-        <View style={style.oval}><Image source={require("../assets/Icons/notification.png")} style={style.icon}/></View>
-        <View style={style.oval}><Image source={require("../assets/Icons/user.png")} style={style.icon}/></View>
-
+      <Image
+        source={require("../assets/Icons/Vector.png")}
+        style={style.apple}
+      />
+      <Text style={style.tituloHeader}>iStore</Text>
+      <View style={style.oval}>
+        <Image
+          source={require("../assets/Icons/notification.png")}
+          style={style.icon}
+        />
+      </View>
+      <View style={style.oval}>
+        <Image
+          source={require("../assets/Icons/user.png")}
+          style={style.icon}
+        />
+      </View>
     </View>
   );
 }
 
 const style = StyleSheet.create({
   container: {
-    paddingTop:50,
-    backgroundColor:"#E5E5E5",
-    paddingLeft:50,
-    paddingRight:140,
+    paddingTop: 50,
+    backgroundColor: "#E5E5E5",
+    paddingLeft: 50,
+    paddingRight: 140,
     width: "100%",
     height: 120,
-    justifyContent:"space-between",
-    alignItems:"center",
-    flexDirection:"row"
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
   },
   tituloHeader: {
     width: "100%",
     color: "black",
     fontSize: 20,
-    fontWeight:"bold",
-    padding:5,
-    marginTop:5,
+    fontWeight: "bold",
+    padding: 5,
+    marginTop: 5,
   },
   apple: {
     width: 23,
-    height: 27,  
-},
-icon:{
+    height: 27,
+  },
+  icon: {
     width: 22,
-    height: 22,  
-},
-  oval:{
-    alignItems:"center",
-    justifyContent:"center",
-    backgroundColor:"#FFFFFF",
-    borderRadius:30,
-    margin:3,
-    width:37,
-    height:37,
-  }
+    height: 22,
+  },
+  oval: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 30,
+    margin: 3,
+    width: 37,
+    height: 37,
+  },
 });
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,17 +1,14 @@
-import React from 'react'
-import { Text, View ,StyleSheet,Image } from 'react-native'
+import React from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+import Categorie from "../Components/Categorie";
 
 export default function Home() {
-  return (
-    <View style={style.body}>
-       
-    </View>
-  )
+  return <View style={style.body}></View>;
 }
 
 const style = StyleSheet.create({
-    body:{
-        backgroundColor:"#E5E5E5",
-        flex:1
-    },
-})
+  body: {
+    backgroundColor: "#E5E5E5",
+    flex: 1,
+  },
+});
